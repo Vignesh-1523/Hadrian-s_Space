1ebf5007632865721be492a6ecc9b476287f9a18
Vignesh-1523 <gabrielmac0912@gmail.com>
2024-01-07 06:10:10 -0800

    scrambledText.js is modified

--- scrambledText.js
+++ scrambledText.js
@@ -13,8 +13,8 @@ class TextScramble {
       for (let i = 0; i < length; i++) {
         const from = oldText[i] || ''
         const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 100)
-        const end = start + Math.floor(Math.random() * 100)
+        const start = Math.floor(Math.random() * 30)
+        const end = start + Math.floor(Math.random() * 30)
         this.queue.push({ from, to, start, end })
       }
       cancelAnimationFrame(this.frameRequest)
@@ -69,7 +69,7 @@ class TextScramble {
   let counter = 0
   const next = () => {
     fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 3000)
+      setTimeout(next, 2000)
     })
     counter = (counter + 1) % phrases.length
   }
